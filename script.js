f61b8803e966907451105b725930922c0d014346
Thejagi67 <singhalabhi876@gmail.com>
2026-09-25 12:56:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   alert('Thank you! Your message has been received.');
   form.reset();
+});const menuButton = document.querySelector(".menu");
+const nav = document.querySelector(".site-header nav");
+
+menuButton.addEventListener("click", () => {
+  nav.classList.toggle("open");
 });
